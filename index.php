e3264c9a87c70c7a5366c6ef9754bc34b221c60e
Benoit Martel <martel.b@gmail.com>
2019-12-05 14:53:51 -0500

    colors

--- index.php
+++ index.php
@@ -25,8 +25,9 @@
 		  <div class="log-info">
     <!-- logged in user information -->
     <?php  if (isset($_SESSION['username'])) : ?>
-    	<div>Bienvenue <strong><?php echo $_SESSION['username']; ?></strong></div>
-    	<div> <a href="index.php?logout='1'"><i class="fa fa-sign-out logout"></i></a> </div>
+		<div> <a href="index.php?logout='1'"><i class="fa fa-sign-out logout"></i></a> </div>
+		    	<div>Bienvenue <strong><?php echo $_SESSION['username']; ?></strong></div>
+
 	<?php endif ?>
 </div>
 </header>
