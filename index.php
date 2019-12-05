162875feda0da589af2f3b650031215a8041e047
Benoit Martel <martel.b@gmail.com>
2019-12-05 15:06:54 -0500

    float

--- index.php
+++ index.php
@@ -24,9 +24,10 @@
   	<header>
 		  <div class="log-info">
     <!-- logged in user information -->
-    <?php  if (isset($_SESSION['username'])) : ?>
+	<?php  if (isset($_SESSION['username'])) : ?>
+			    	<div>Bienvenue <strong><?php echo $_SESSION['username']; ?></strong></div>
 		<div> <a href="index.php?logout='1'"><i class="fa fa-sign-out logout"></i></a> </div>
-		    	<div>Bienvenue <strong><?php echo $_SESSION['username']; ?></strong></div>
+
 
 	<?php endif ?>
 </div>
