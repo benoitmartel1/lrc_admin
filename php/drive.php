19d974571f44a7b318acf83c58e9b6ad3918b167
Benoit Martel <martel.b@gmail.com>
2019-12-18 09:32:24 -0500

    css replace test

--- php/drive.php
+++ php/drive.php
@@ -1,6 +1,8 @@
 <?php
 $content = file_get_contents('https://drive.google.com/embeddedfolderview?id=1hsuRy_H6ATRs7XRrLxH14Yrh5YLEx-ix#list');
-echo $content;
+$content = str_replace("/static","https://drive.google.com/static". $content);
+echo 
+
 
 
 
