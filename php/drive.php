993eb2350bdfa71e39f60d2ff71cd544141f7dc8
Benoit Martel <martel.b@gmail.com>
2019-12-18 09:33:04 -0500

    ,

--- php/drive.php
+++ php/drive.php
@@ -1,6 +1,6 @@
 <?php
 $content = file_get_contents('https://drive.google.com/embeddedfolderview?id=1hsuRy_H6ATRs7XRrLxH14Yrh5YLEx-ix#list');
-$content = str_replace("/static","https://drive.google.com/static". $content);
+$content = str_replace("/static","https://drive.google.com/static", $content);
 echo 
 
 
