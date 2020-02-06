4e3e67d2d71af2c2a0172fa3d4462d8ecdcdc4a9
Benoit Martel <martel.b@gmail.com>
2020-02-06 10:57:39 -0500

    list add

--- search.php
+++ search.php
@@ -7,15 +7,53 @@
 	<title>LRC - Panneau de contrôle</title>
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <script src="js/jquery-3.4.1.js"></script>
+<script src="js/list.min.js"></script>
 <script>
 var programs = <?php echo json_encode($programs); ?>;
 console.log(programs);
+var options = {
+  valueNames: [ 'name', 'price' ]
+};
+
+
+
+var userList = new List('users', options);
 </script>
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 
 <body>
+<div id="users">
 
+<!-- class="search" automagically makes an input a search field. -->
+  <input class="search" placeholder="Search" />
+<!-- class="sort" automagically makes an element a sort buttons. The date-sort value decides what to sort by. -->
+  <button class="sort" data-sort="name">
+    Sort
+  </button>
+
+<!-- Child elements of container with class="list" becomes list items -->
+  <ul class="list">
+    <li>
+<!-- The innerHTML of children with class="name" becomes this items "name" value -->
+      <h3 class="name">Jonny Stromberg</h3>
+      <p class="born">1986</p>
+    </li>
+    <li>
+      <h3 class="name">Jonas Arnklint</h3>
+      <p class="born">1985</p>
+    </li>
+    <li>
+      <h3 class="name">Martina Elm</h3>
+      <p class="born">1986</p>
+    </li>
+    <li>
+      <h3 class="name">Gustaf Lindqvist</h3>
+      <p class="born">1983</p>
+    </li>
+  </ul>
+
+</div>
 </body>
 </html>
