6f7cb46b4ab1b4cf7e2749d8f4064edd3b4fde7d
Benoit Martel <martel.b@gmail.com>
2020-02-06 08:29:58 -0500

    json test

--- php/activities.php
+++ php/activities.php
@@ -5,7 +5,7 @@ $auth_url = 'https://www.amilia.com/api/v3/authenticate';
 
 $curl = curl_init();
 
-//Make the request to the API
+//Request to the API
 function sendHTTPrequest($curl, $url, $headers){
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
@@ -19,25 +19,27 @@ $token=sendHTTPrequest($curl, $auth_url, $auth_data);
 $token=json_decode($token)->Token;
 
 if(!$token){die("Connection Failure");}else{
+
+	//If the token is obtained, then make the request to the API
     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs';
     $auth_data = array('Authorization: Bearer '.$token);
-    $programs=sendHTTPrequest($curl, $auth_url, $auth_data);
+	$result=sendHTTPrequest($curl, $auth_url, $auth_data);
+	echo $result;
+	// $items=json_decode($result)->Items;
 
-    $pr=json_decode($programs);
-    $items=$pr->Items;
-
-    foreach($items as $program){
-        $id=$program->Id;
-        //echo $program->Name.'<br><br>';
-        $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities';
-        $auth_data = array('Authorization: Bearer '.$token);
-        $result=sendHTTPrequest($curl, $auth_url, $auth_data);
-        $activities=json_decode($result);
-      $activities=$activities->Items;
-      foreach($activities as $activity){
-          echo $activity->Name.'<br>';
-      }
-    };
+	//Process the returned items
+    // foreach($items as $item){
+    //     $id=$item->Id;
+    //     //echo $item->Name.'<br><br>';
+    //     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities';
+    //     $auth_data = array('Authorization: Bearer '.$token);
+    //     $result=sendHTTPrequest($curl, $auth_url, $auth_data);
+    //     $activities=json_decode($result);
+    //   $activities=$activities->Items;
+    //   foreach($activities as $activity){
+    //       echo $activity->Name.'<br>';
+    //   }
+    // };
 };
 
 curl_close($curl);
