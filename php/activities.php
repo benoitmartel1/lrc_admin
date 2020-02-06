cb707f0bdb75046c6f41dc00b1bd4cad294c7506
Benoit Martel <martel.b@gmail.com>
2020-02-06 10:38:30 -0500

    echo

--- php/activities.php
+++ php/activities.php
@@ -39,6 +39,7 @@ if(!$token){die("Connection Failure");}else{
 		//Add the activities array to property Activities of currently processed Program.
 		$program->Activities=$activities;
 	};
+	echo $programs;
 	// $programsArray=object_to_array($programs);
 	//$programs=json_encode($programs);
 };
