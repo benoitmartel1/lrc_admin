1ea04e5d0627f4e07d93833b841ba4a58036642b
Benoit Martel <martel.b@gmail.com>
2020-02-06 08:25:17 -0500

    Token

--- php/activities.php
+++ php/activities.php
@@ -1,8 +1,12 @@
 <?php
+//Authorization obtained through Postman -- https://www.amilia.com/ApiDocs
+$auth_data = array('Authorization: Basic bWFydGVsLmJAZ21haWwuY29tOmxhdXJlbnRtNw==');
+$auth_url = 'https://www.amilia.com/api/v3/authenticate';
+
 $curl = curl_init();
 
+//Make the request to the API
 function sendHTTPrequest($curl, $url, $headers){
-
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
@@ -10,16 +14,13 @@ function sendHTTPrequest($curl, $url, $headers){
     return $result;
 };
 
-$auth_data = array('Authorization: Basic bWFydGVsLmJAZ21haWwuY29tOmxhdXJlbnRtNw==');
-$auth_url = 'https://www.amilia.com/api/v3/authenticate';
-
-
+//Get token
 $token=sendHTTPrequest($curl, $auth_url, $auth_data);
-$token=json_decode($token);
+$token=json_decode($token)->Token;
 
 if(!$token){die("Connection Failure");}else{
     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs';
-    $auth_data = array('Authorization: Bearer '.$token->Token);
+    $auth_data = array('Authorization: Bearer '.$token);
     $programs=sendHTTPrequest($curl, $auth_url, $auth_data);
 
     $pr=json_decode($programs);
@@ -29,7 +30,7 @@ if(!$token){die("Connection Failure");}else{
         $id=$program->Id;
         //echo $program->Name.'<br><br>';
         $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities';
-        $auth_data = array('Authorization: Bearer '.$token->Token);
+        $auth_data = array('Authorization: Bearer '.$token);
         $result=sendHTTPrequest($curl, $auth_url, $auth_data);
         $activities=json_decode($result);
       $activities=$activities->Items;
