c805ed6094d2b0301f2eae9d9d51ef596314bd1b
Benoit Martel <martel.b@gmail.com>
2020-02-06 10:28:41 -0500

    json.parse

--- php/activities.php
+++ php/activities.php
@@ -39,24 +39,24 @@ if(!$token){die("Connection Failure");}else{
 		//Add the activities array to property Activities of currently processed Program.
 		$program->Activities=$activities;
 	};
-	$programsArray=object_to_array($programs);
+	// $programsArray=object_to_array($programs);
 	$programs=json_encode($programsArray);
 };
 curl_close($curl);
 
-function object_to_array($data)
-{
-    if(is_array($data) || is_object($data))
-    {
-        $result = array();
+// function object_to_array($data)
+// {
+//     if(is_array($data) || is_object($data))
+//     {
+//         $result = array();
  
-        foreach($data as $key => $value) {
-            $result[$key] = $this->object_to_array($value);
-        }
+//         foreach($data as $key => $value) {
+//             $result[$key] = $this->object_to_array($value);
+//         }
  
-        return $result;
-    }
+//         return $result;
+//     }
  
-    return $data;
-}
+//     return $data;
+// }
 ?>
