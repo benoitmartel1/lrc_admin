d3d7ac3f801b55fd13ce339472368ead94021cea
Benoit Martel <martel.b@gmail.com>
2020-02-06 11:37:03 -0500

    fix

--- search.php
+++ search.php
@@ -8,19 +8,33 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script src="https://listjs.com/assets/javascripts/list.min.js"></script>
 <script src="js/jquery-3.4.1.js"></script>
-<script src="js/list.min.js"></script>
+
 <script>
 var programs = <?php echo json_encode($programs); ?>;
 console.log(programs);
 
 var options = {
-  valueNames: [ 'name', 'price' ]
+  valueNames: [ 'name', 'price' ],
+  item: '<li><h3 class="name"></h3><p class="price"></p></li>' 
 };
-$(document).ready(function(){
-	console.log('ready');
-var userList = new List('users', options);
-});
+var values = [{ 
+    name: 'Jonny Strömberg', 
+    born: 1986 
+  }, 
+  { 
+    name: 'Jonas Arnklint', 
+    born: 1985 
+  }, 
+  { 
+    name: 'Martina Elm', 
+    born: 1986 
+}]; 
+
+
+var userList = new List('users', options, values);
+
 
 
 
@@ -39,13 +53,7 @@ var userList = new List('users', options);
   </button>
 
 <!-- Child elements of container with class="list" becomes list items -->
-  <ul class="list">
-    <li>
-<!-- The innerHTML of children with class="name" becomes this items "name" value -->
-      <h3 class="name">Jonny Stromberg</h3>
-      <p class="price">1986</p>
-    </li>
-  </ul>
+  <ul class="list"></ul>
 
 </div>
 </body>
