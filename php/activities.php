aec6b4e8d4c937ee2d23250334910bad530eab55
Benoit Martel <martel.b@gmail.com>
2020-02-06 10:32:06 -0500

    programsArray remove

--- php/activities.php
+++ php/activities.php
@@ -40,7 +40,7 @@ if(!$token){die("Connection Failure");}else{
 		$program->Activities=$activities;
 	};
 	// $programsArray=object_to_array($programs);
-	$programs=json_encode($programsArray);
+	$programs=json_encode($programs);
 };
 curl_close($curl);
 
