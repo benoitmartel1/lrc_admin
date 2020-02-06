736cfa962b39d89f3bb0560b16f8a99469ad20a5
Benoit Martel <martel.b@gmail.com>
2020-02-06 11:06:35 -0500

    value

--- search.php
+++ search.php
@@ -15,9 +15,10 @@ var options = {
   valueNames: [ 'name', 'price' ]
 };
 
+var values = [
+];
 
-
-var userList = new List('users', options);
+var userList = new List('users', options, values);
 </script>
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
