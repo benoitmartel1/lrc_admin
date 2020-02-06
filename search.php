7706db5645c70259ea13fa98c98029a0ab38859d
Benoit Martel <martel.b@gmail.com>
2020-02-06 09:00:20 -0500

    json tweak

--- search.php
+++ search.php
@@ -8,8 +8,8 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 <script src="js/jquery-3.4.1.js"></script>
 <script>
-var programs = "<?php echo $programs ?>";
-console.log(programs);
+var programs = "<?php echo json_encode($programs) ?>";
+console.log($.parseJSON(programs));
 </script>
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
