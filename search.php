d1cbd0346f258fa82221ea107dfd9021d5a462a1
Benoit Martel <martel.b@gmail.com>
2020-02-07 10:36:00 -0500

    popper

--- search.php
+++ search.php
@@ -8,9 +8,10 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
-
+	<script src="js/jquery-3.4.1.js"></script>
+<script src="js/popper.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/list.js/1.5.0/list.min.js"></script>
-<script src="js/jquery-3.4.1.js"></script>
+
 <script src="js/text_FR.js"></script>
 <script>
 var programs = <?php echo json_encode($programs); ?>;
