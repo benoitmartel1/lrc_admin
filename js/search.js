976b32fed8139f9d8704041102e1e5797c63be03
Benoit Martel <martel.b@gmail.com>
2020-02-14 08:41:01 -0500

    context

--- js/search.js
+++ js/search.js
@@ -72,10 +72,10 @@ $(function() {
   });
 
   //-- When you click on li item, the div with details info toggles.
-  $("li").click(event => {
-    event.stopPropagation();
-    console.log($(event.target));
-    $(".details", event.target).slideToggle("fast");
+  $("li").click(() => {
+    var context = $(this).closest("li");
+    console.log($(this).closest("li"));
+    $(".details", context).slideToggle("fast");
   });
 
   var options = {
