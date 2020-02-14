01f3378ce126218c50ccfd62374fc6434737a67d
Benoit Martel <martel.b@gmail.com>
2020-02-14 11:47:08 -0500

    ;

--- php/activities.php
+++ php/activities.php
@@ -50,7 +50,7 @@ if(!$token){die("Connection Failure");}else{
 	        	$auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/activities/'.$id.'/persons';
         		$persons=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 			foreach($persons as $person){
-				print_r($person->FullName)
+				print_r($person->FullName);
 			};
 				   echo "</pre>";
 			}
