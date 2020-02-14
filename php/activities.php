7dfe315e1a197d8ffd30e7f1d007f008b91866a0
Benoit Martel <martel.b@gmail.com>
2020-02-14 10:32:25 -0500

    token

--- php/activities.php
+++ php/activities.php
@@ -13,19 +13,19 @@ function sendHTTPrequest($curl, $url, $headers){
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
     $result=curl_exec($curl);
-    return json_decode($result)->Items;
+    return json_decode($result);
 };
 
 //Get token
-$token=sendHTTPrequest($curl, $auth_url, $auth_data);
-$token=json_decode($token)->Token;
+$token=sendHTTPrequest($curl, $auth_url, $auth_data)->Token;
+// $token=json_decode($token)->Token;
 
 if(!$token){die("Connection Failure");}else{
 
 	//If the token is obtained, then make the request to the API
     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs';
     $auth_data = array('Authorization: Bearer '.$token);
-	$programs=sendHTTPrequest($curl, $auth_url, $auth_data);
+	$programs=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 	
 	// $programs=json_decode($result)->Items;
 
@@ -33,7 +33,7 @@ if(!$token){die("Connection Failure");}else{
     foreach($programs as $program){
         $id=$program->Id;
         $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities';
-        $activities=sendHTTPrequest($curl, $auth_url, $auth_data);
+        $activities=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 		// $activities=json_decode($result)->Items;
 		//Add the activities array to property Activities of currently processed Program.
 		$program->Activities=$activities;
@@ -41,7 +41,7 @@ if(!$token){die("Connection Failure");}else{
 
 
 	        $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/staff';
-			$staff=sendHTTPrequest($curl, $auth_url, $auth_data);
+			$staff=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 			echo $staff;
 
 };
