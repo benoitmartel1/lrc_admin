56e1dc55076a521fb540969c55df5f9aabc444a5
Benoit Martel <martel.b@gmail.com>
2020-02-14 11:29:11 -0500

    participânts

--- php/activities.php
+++ php/activities.php
@@ -43,7 +43,7 @@ if(!$token){die("Connection Failure");}else{
 	        $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/events?from=2020-01-01&to=2020-12-30';
 			$staff=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 			foreach($staff as $item){
-				echo "<pre>"; print_r($item->Activity->Id); print_r($item->Staff); echo "</pre>";
+				echo "<pre>"; print_r($item->Activity->Id); print_r($item->Participants); echo "</pre>";
 			}
 
 
