8fe569f478124157c39a661eb30a73044b5d76a0
Benoit Martel <martel.b@gmail.com>
2020-02-19 16:09:58 -0500

    char fix

--- php/staff.php
+++ php/staff.php
@@ -2,12 +2,12 @@
 $db = mysqli_connect('localhost', 'renaudcoursol', 'xuY8#VdvNQE8', 'renaudcoursol_admin');
   
 /* change character set to utf8 */
-if (!$db->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $db->error);
-    exit();
-} else {
-    printf("Current character set: %s\n", $db->character_set_name());
-}
+// if (!$db->set_charset("utf8")) {
+//     printf("Error loading character set utf8: %s\n", $db->error);
+//     exit();
+// } else {
+//     printf("Current character set: %s\n", $db->character_set_name());
+// }
 
 
 $user_check_query = "SELECT nom, prenom, email, phone, cell FROM staff";
