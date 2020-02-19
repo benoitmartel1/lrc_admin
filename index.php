84f99ee4e68ae1b42ff4c11e3f7e05dce2273082
Benoit Martel <martel.b@gmail.com>
2020-02-19 16:15:07 -0500

    js loop

--- index.php
+++ index.php
@@ -23,9 +23,15 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
 <script>
 var staff = <?php echo json_encode($staff); ?>;
-console.log(staff);
+$(staff).each(()=>{
+	console.log(this.name);
+});
 </script>
 
 </head>
