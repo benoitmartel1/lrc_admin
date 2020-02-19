5f0322faf35523ab8fada564c28e11716db8823c
Benoit Martel <martel.b@gmail.com>
2020-02-19 15:53:14 -0500

    staff

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <script>
-var programs = <?php echo $programs; ?>;
-console.log(programs);
+var staff = <?php echo $staff; ?>;
+console.log(staff);
 </script>
 
 </head>
