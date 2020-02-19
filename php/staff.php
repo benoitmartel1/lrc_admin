dbc53cdab07aba5840802eecc4c4fe2e9d7ebb31
Benoit Martel <martel.b@gmail.com>
2020-02-19 15:41:26 -0500

    staff

--- php/staff.php
+++ php/staff.php
@@ -1,12 +1,6 @@
 <?php
 $db = mysqli_connect('localhost', 'renaudcoursol', 'xuY8#VdvNQE8', 'renaudcoursol_admin');
-  $user_check_query = "SELECT 
-  nom, 
-  prenom, 
-  email,
-  phone,
-  cell 
-  FROM staff";
+  $user_check_query = "SELECT nom, prenom, email,phone,cell FROM staff";
   $result = mysqli_query($db, $user_check_query);
-$staff = mysqli_fetch_all ($result, MYSQLI_ASSOC);
+$staff = mysqli_fetch_all($result, MYSQLI_ASSOC);
 ?>
