088ad75475e54b94055d67ab064b10bd50313e81
Benoit Martel <martel.b@gmail.com>
2020-02-19 15:50:12 -0500

    echp

--- index.php
+++ index.php
@@ -22,8 +22,8 @@
 	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <script>
-
-// console.log(staff);
+var programs = <?php echo $programs; ?>;
+console.log(programs);
 </script>
 
 </head>
@@ -41,7 +41,7 @@
 		</div>
 	</div>
 
-<?php print_r($staff); ?>;
+
 
 
 </div>
