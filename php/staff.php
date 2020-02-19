9318e18ee4f9273e893c0990abcc3c8ad734948f
Benoit Martel <martel.b@gmail.com>
2020-02-19 16:02:08 -0500

    removal of login

--- php/staff.php
+++ php/staff.php
@@ -1,6 +1,6 @@
 <?php
 $db = mysqli_connect('localhost', 'renaudcoursol', 'xuY8#VdvNQE8', 'renaudcoursol_admin');
-  $user_check_query = "SELECT nom, prenom, email,phone,cell FROM staff";
+  $user_check_query = "SELECT nom, prenom, email, phone, cell FROM staff";
   $result = mysqli_query($db, $user_check_query);
-$staff = $result -> fetch_array(MYSQLI_ASSOC);
+$staff = $result->fetch_all( MYSQLI_ASSOC );
 ?>
