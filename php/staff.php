905a095ef34d21606877d56b1b53aba549e0bb7d
Benoit Martel <martel.b@gmail.com>
2020-02-19 15:56:49 -0500

    pdo

--- php/staff.php
+++ php/staff.php
@@ -2,5 +2,5 @@
 $db = mysqli_connect('localhost', 'renaudcoursol', 'xuY8#VdvNQE8', 'renaudcoursol_admin');
   $user_check_query = "SELECT nom, prenom, email,phone,cell FROM staff";
   $result = mysqli_query($db, $user_check_query);
-$staff = mysqli_fetch_all($result, MYSQLI_ASSOC);
+$staff = $result -> fetch_array(MYSQLI_ASSOC);
 ?>
