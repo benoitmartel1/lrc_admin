4918ba1369000bf3f837775b883987a0a3b2805f
Benoit Martel <martel.b@gmail.com>
2020-02-20 13:54:22 -0500

    header

--- edit.php
+++ edit.php
@@ -1,16 +1,6 @@
 <?php 
-  session_start(); 
-
-  if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: registration/login.php');
-  }
-  if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: registration/login.php");
-  }
-
+//Login Check
+include('header.php');
   //db connect  
 include('db_connect.php');
 
