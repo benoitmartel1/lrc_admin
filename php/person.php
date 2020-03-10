1d1eca35f3a05d53adab78fb292e480fc65e9844
Benoit Martel <martel.b@gmail.com>
2020-03-10 11:20:54 -0400

    remove delete

--- php/person.php
+++ php/person.php
@@ -5,14 +5,12 @@ $person_id=$_POST['id'];
 $name=$db->real_escape_string($_POST['name']);
 $surname=$db->real_escape_string($_POST['surname']);
 
-$query="";
-
-function updatePerson($person_id, $name, $surname){
 $query="UPDATE person SET name='$name', surname='$surname' WHERE id=$person_id";
-}
-function deletePerson($person_id){
-$query="DELETE FROM person WHERE person=$person_id";
-}
+
+//A mettre ailleurs
+// function deletePerson($person_id){
+// $query="DELETE FROM person WHERE person=$person_id";
+// }
 
 if (!$db -> query($query)) {
   echo("Error description: " . $db -> error);
