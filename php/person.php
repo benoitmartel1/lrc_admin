72f975586b882befcfe67ba88695d9b4850c1209
Benoit Martel <martel.b@gmail.com>
2020-03-10 12:29:20 -0400

    replace into syntax

--- php/person.php
+++ php/person.php
@@ -2,6 +2,7 @@
 include('../db_connect.php');
 
 $person_id=$_POST['id'];
+//Check if id is set else 
 $name=$db->real_escape_string($_POST['name']);
 $surname=$db->real_escape_string($_POST['surname']);
 $phone=$db->real_escape_string($_POST['phone']);
@@ -18,8 +19,7 @@ $pdq_status=$db->$_POST['pdq_status'];
 $pdq_delivery_date=$db->real_escape_string($_POST['pdq_delivery_date']);
 $is_rcr=$db->$_POST['is_rcr'];
 
-$query="REPLACE INTO person (id, name, surname, phone, cell, work, address, city, zipcode, birthdate, creation_date, employee_number, pdq_number, pdq_status, pdq_delivery_date, is_rcr) 
-VALUES ($person_id, '$name','$surname','$phone','$cell','$work','$address','$city','$zipcode',NULLIF('$birthdate',''),NULLIF('$creation_date',''),'$employee_number','$pdq_number',NULLIF('$pdq_status',''),NULLIF('$pdq_delivery_date',''),NULLIF('$is_rcr','')";
+$query="REPLACE INTO person (id, name, surname, phone, cell, work, address, city, zipcode, birthdate, creation_date, employee_number, pdq_number, pdq_status, pdq_delivery_date, is_rcr) VALUES ($person_id, '$name','$surname','$phone','$cell','$work','$address','$city','$zipcode',NULLIF('$birthdate',''),NULLIF('$creation_date',''),'$employee_number','$pdq_number',NULLIF('$pdq_status',''),NULLIF('$pdq_delivery_date',''),NULLIF('$is_rcr','')";
 
 if (!$db -> query($query)) {
   echo("Error description: " . $db -> error);
