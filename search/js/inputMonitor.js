533b567f07cf5f9aa8ce319ee00aac6408092abf
Benoit Martel <martel.b@gmail.com>
2020-08-24 12:00:34 -0400

    timer

--- search/js/inputMonitor.js
+++ search/js/inputMonitor.js
@@ -1,8 +1,26 @@
 $( document ).ready(function() {
+
+
+
+	var Timer;
+
+function chChannel(pressed) {
+   
+
+    // call function to change channel using variable value inputChannel for ex. 011
+};
+
 	console.log('ready');
 	$('input').on('input', function(){
-		console.log(this);
-		console.log($(this).val());
+		console.log('start');
+
+		 clearTimeout(Timer);
+		Timer = setTimeout(function () {
+			console.log('time out');
+		if ($(this).val().length() > 2) {
+			console.log($(this).val());
+		}
+		}, 2000);
 	})
 
 });
