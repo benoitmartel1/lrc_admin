2e4e2499c8aeff498104a51fb68ffe74849ff899
Benoit Martel <martel.b@gmail.com>
2020-08-24 12:01:51 -0400

    timer fix

--- search/js/inputMonitor.js
+++ search/js/inputMonitor.js
@@ -1,26 +1,12 @@
 $( document ).ready(function() {
-
-
-
 	var Timer;
 
-function chChannel(pressed) {
-   
-
-    // call function to change channel using variable value inputChannel for ex. 011
-};
-
-	console.log('ready');
 	$('input').on('input', function(){
-		console.log('start');
-
 		 clearTimeout(Timer);
 		Timer = setTimeout(function () {
-			console.log('time out');
-		if ($(this).val().length() > 2) {
+		if ($(this).val().length > 2) {
 			console.log($(this).val());
 		}
 		}, 2000);
 	})
-
 });
