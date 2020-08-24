36d2b955629e85c4420b067dbd4ea917a81d2248
Benoit Martel <martel.b@gmail.com>
2020-08-24 12:09:05 -0400

    entry check

--- search/js/inputMonitor.js
+++ search/js/inputMonitor.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {
 	var Timer;
-console.log(2);
+	
 	$('input').on('input', function(){
 		var entry=$(this).val();
-		console.log(entry);
-		 clearTimeout(Timer);
+		clearTimeout(Timer);
 		Timer = setTimeout(function () {
 		if (entry.length > 2) {
 			console.log(entry);
