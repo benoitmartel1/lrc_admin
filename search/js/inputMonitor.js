0b3b929be85f309170759c16840b794ec2f7e1fc
Benoit Martel <martel.b@gmail.com>
2020-08-24 12:06:45 -0400

    string

--- search/js/inputMonitor.js
+++ search/js/inputMonitor.js
@@ -1,11 +1,12 @@
 $( document ).ready(function() {
 	var Timer;
-
+console.log(2);
 	$('input').on('input', function(){
 		var entry=$(this).val();
+		console.log(entry);
 		 clearTimeout(Timer);
 		Timer = setTimeout(function () {
-		if ($(entry).length > 2) {
+		if (entry.length > 2) {
 			console.log(entry);
 		}
 		}, 2000);
