e975397487dea9e857972c9854393fafeb13bf05
Benoit Martel <martel.b@gmail.com>
2020-08-25 08:36:32 -0400

    check connect

--- search/php/insertTracker.php
+++ search/php/insertTracker.php
@@ -2,6 +2,12 @@
 //db connect  
 include('../../db_connect.php');
 
+if ($db) {
+  echo 'connected';
+} else {
+  echo 'not connected';
+}
+
 $session_id=$_POST['sessionId'];
 $type=$db->real_escape_string($_POST['type']);
 $value=$db->real_escape_string($_POST['value']);
@@ -10,6 +16,7 @@ $query = "INSERT INTO `tracker_input`(`session_id`, `value`) VALUES ($session_id
 $result = mysqli_query($db, $query);
 
 echo $value;
+echo $session_id;
 echo $result;
 
 ?>
