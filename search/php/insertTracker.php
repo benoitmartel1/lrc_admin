09004c3ba0ab53e2195fb2104cff92d0cb32499d
Benoit Martel <martel.b@gmail.com>
2020-08-25 08:58:02 -0400

    column name

--- search/php/insertTracker.php
+++ search/php/insertTracker.php
@@ -12,12 +12,12 @@ $session_id=$_POST['sessionId'];
 $type=$db->real_escape_string($_POST['type']);
 $val=$db->real_escape_string($_POST['value']);
 
-$query = "INSERT INTO tracker_input (session_id, val) VALUES ('$session_id', '$value')";
+$query = "INSERT INTO tracker_input (session_id, value) VALUES ('$session_id', '$val')";
 
 if ($db->query($query) === TRUE) {
   echo "New record created successfully";
 } else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+  echo "Error: " . $query . "<br>" . $db->error;
 }
 
 $db->close();
