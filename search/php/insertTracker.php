8e20fbb8f6c4abd0f9b57b1a65e9db4ca8c05568
Benoit Martel <martel.b@gmail.com>
2020-08-25 08:50:05 -0400

    quotes

--- search/php/insertTracker.php
+++ search/php/insertTracker.php
@@ -12,7 +12,7 @@ $session_id=$_POST['sessionId'];
 $type=$db->real_escape_string($_POST['type']);
 $value=$db->real_escape_string($_POST['value']);
 
-$query = "INSERT INTO `tracker_input`(`session_id`, `value`) VALUES (999, $value)";
+$query = "INSERT INTO tracker_input (session_id`, value) VALUES ($session_id, $value)";
 $result = mysqli_query($db, $query);
 
 echo $value;
