77c635fe63d300eaceec54c7f055f7c51621f1cf
Benoit Martel <martel.b@gmail.com>
2020-08-25 08:16:22 -0400

    tracker init

--- search/js/inputMonitor.js
+++ search/js/inputMonitor.js
@@ -1,13 +1,32 @@
 $( document ).ready(function() {
+
+	var sessionId=defineSessionId();
 	var Timer;
 
+	//Listeners
 	$('input').on('input', function(){
 		var entry=$(this).val();
 		clearTimeout(Timer);
 		Timer = setTimeout(function () {
 		if (entry.length > 2) {
 			console.log(entry);
+			inputTracker(sessionId, entry);
 		}
 		}, 1500);
 	})
 });
+
+function defineSessionId(){
+	var id=999;
+	return id;
+};
+function inputTracker(sessionId, str) {
+	$.post('php/insertTracker.php',{
+		sessionId:sessionId,
+		type:'input',
+		value:str
+	});
+};
+function viewActivityTracker(sessionId, id) {};
+function filterTracker(sessionId, type, value) {};
+function signUpTracker(sessionId, id) {};
---
+++ search/php/insertTracker.php
@@ -0,0 +1,12 @@
+<?php
+//db connect  
+include('../../db_connect.php');
+
+$session_id=$_POST['sessionId'];
+$type=$db->real_escape_string($_POST['type']);
+$value=$db->real_escape_string($_POST['value']);
+
+$query = "INSERT INTO `tracker_input`(`session_id`, `value`) VALUES ($session_id, $value)";
+$result = mysqli_query($db, $query);
+
+?>
