e0f2d9e1bd11a661dd6cc51205ef2afa0ef8d2e9
Benoit Martel <martel.b@gmail.com>
2020-08-28 15:25:19 -0400

    tr

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli('localhost', 'renaudcoursol_trackers', 'q9TtuYV8Pq2$', 'renaudcoursol_admin');
+$db = new mysqli('localhost', 'renaudcoursol', 'q9TtuYV8Pq2$', 'renaudcoursol_admin');
   /* change character set to utf8 */
 if (!$db->set_charset("utf8")) {
     printf("Error loading character set utf8: %s\n", $db->error);
