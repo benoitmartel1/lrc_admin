e773e7afd7a504a7e4139a3b8ac7f255031d50b3
Benoit Martel <martel.b@gmail.com>
2020-08-31 09:43:17 -0400

    session 2

--- search/js/routines.js
+++ search/js/routines.js
@@ -67,16 +67,13 @@ function formatSession(sDate,eDate){
 	var e=eDate.getMonth();
 	switch (true) {
     case s < 3:
-	  return '<i class="fal fa-snowflake" aria-hidden="true"></i>' + 
-	  text.sessions.winter;
+	  return '<i class="fal fa-snowflake" aria-hidden="true"></i>' + text.sessions.winter;
       break;
     case s < 6:
-      return '<i class="fal fa-sun" aria-hidden="true"></i>' +
-        text.sessions.spring;
+      return '<i class="fal fa-sun" aria-hidden="true"></i>' + text.sessions.spring;
       break;
     case s > 6 && e > 6:
-      return '<i class="fal fa-leaf" aria-hidden="true"></i>' +
-        text.sessions.fall;
+      return '<i class="fal fa-leaf" aria-hidden="true"></i>' + text.sessions.fall;
       break;
     case s > 6 && e < 6:
       return text.sessions.yearly;
