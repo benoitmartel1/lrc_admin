079e351ef33a6e50c2593fe4c1974fce268bd976
Benoit Martel <martel.b@gmail.com>
2020-09-03 16:06:21 -0400

    body only

--- search/index.php
+++ search/index.php
@@ -1,35 +1,4 @@
-<head>
-	<base href="https://renaudcoursol.com/admin/search/">
 
-	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-	<link rel="stylesheet" type="text/css" href="css/search.css">
-	<link rel="stylesheet" type="text/css" href="css/media.css">
-	<!--[if lt IE 9]>
-	<link rel="stylesheet" type="text/css" href="css-es5/main.css">
-	<link rel="stylesheet" type="text/css" href="css-es5/search.css">
-	<link rel="stylesheet" type="text/css" href="css-es5/media.css">
-	<![endif]-->
-<script
-  src="https://code.jquery.com/jquery-3.4.1.min.js"
-  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-  crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/51b21781c6.js" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	<script src="js-es5/list.min.js"></script>
-	<script src="js-es5/text_fr.js"></script>
-	<script src="js-es5/routines.js"></script>
-	<script src="js-es5/search.js"></script>
-	<script src="js-es5/trackers.js"></script>
-	<script>
-		//Enable version to print format
-		var printable=false;
-		console.log("Printable : "+printable);
-	</script>
-</head>
 
 <body>
 <!-- <div class="black">1</div> -->
