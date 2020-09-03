39a577f244fd4aa14ca4919177f1c68a1bc42939
Benoit Martel <martel.b@gmail.com>
2020-09-03 14:06:34 -0400

    remove set

--- search/js-es5/search.js
+++ search/js-es5/search.js
@@ -1,19 +1,5 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
-
-function _nonIterableSpread() { 
-	//throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); 
-}
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
 var sortCategoriesByName;
 $(document).ready(function () {
   // console.log('js');
@@ -42,16 +28,14 @@ function fillGrid(data, text) {
   var editMode = true;
   var sessionsToDisplay = [text.sessions.spring, // text.sessions.summer,
   text.sessions.fall, text.sessions.winter, text.sessions.yearly];
-
-  var categories = _toConsumableArray(new Set(activities.map(function (n) {
+  var categories = activities.map(function (n) {
     console.log(n);
     var cat = {};
     cat.name = n.CategoryName;
     cat["class"] = convertToClassSafe(n.CategoryName);
     cat.id = n.CategoryId;
     return cat;
-  })));
-
+  });
   console.log("beforeSort"); //Alpha sort
 
   categories.sort(sortCategoriesByName);
