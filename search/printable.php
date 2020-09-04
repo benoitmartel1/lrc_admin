e2cc13f6904072311d44f35e2281f70566e4af18
Benoit Martel <martel.b@gmail.com>
2020-09-04 08:04:34 -0400

    out time

--- search/printable.php
+++ search/printable.php
@@ -19,7 +19,6 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment-with-locales.min.js"></script>
 <script src="js-es5/list.min.js"></script>
-	<script src="js-es5/time-zone.js"></script>
 	<script src="js-es5/text_fr.js"></script>
 	<script src="js-es5/routines.js"></script>
 	<script src="js-es5/search.js"></script>
