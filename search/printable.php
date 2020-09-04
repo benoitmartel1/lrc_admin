4bf8543b9d2c1ff8a43d358387a8697e878b94dd
Benoit Martel <martel.b@gmail.com>
2020-09-04 14:43:18 +0200

    es6

--- search/printable.php
+++ search/printable.php
@@ -17,7 +17,7 @@
   <script src="https://kit.fontawesome.com/51b21781c6.js" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="js-es5/moment-tz.js"></script>
+<script src="js/moment-tz.js"></script>
 	<script src="js-es5/list.min.js"></script>
 	<script src="js-es5/text_fr.js"></script>
 	<script src="js-es5/routines.js"></script>
