8cb27f68ee7e34fa38cad4a59c80b83b9e1b84d4
Benoit Martel <martel.b@gmail.com>
2020-09-09 09:25:55 -0400

    trackers.php

---
+++ search/trackers.php
@@ -0,0 +1,29 @@
+<?php
+//Log Check
+include('../log_check.php');
+//db connect
+include('../db_connect.php');
+
+$query = "SELECT * FROM trackers";
+$result = $db->query($query);
+$trackers = $result->fetch_all(MYSQLI_ASSOC);
+
+var_dump($trackers);
+?>
+<!DOCTYPE html>
+<html>
+<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<title>LRC - Panneau de contrôle 2</title>
+	<link href="https://fonts.googleapis.com/css?family=Nunito:400,800&display=swap" rel="stylesheet">
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/staff.css">
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+</head>
+<body>
+
+</body>
+</html>
