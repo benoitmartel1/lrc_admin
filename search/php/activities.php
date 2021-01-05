71a6b23fae08bb60a3dd2368b5b554b11c99f394
Benoit Martel <martel.b@gmail.com>
2021-01-05 09:34:19 -0500

    cours virtuels

--- search/php/activities.php
+++ search/php/activities.php
@@ -7,76 +7,72 @@ $auth_data = array('Authorization: Basic bWFydGVsLmJAZ21haWwuY29tOmxhdXJlbnRtNw=
 $auth_url = 'https://www.amilia.com/api/v3/authenticate';
 
 $curl = curl_init();
+$allActivities=array();
+$programsToShow=[55462]; //Eventually, get list of programs to show by querying a database so admnin can select which program.
 
 //Request to the API
-function sendHTTPrequest($curl, $url, $headers){
+function sendHTTPrequest($curl, $url, $headers)
+{
     curl_setopt($curl, CURLOPT_URL, $url);
     curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER,1);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
     $result=curl_exec($curl);
     return json_decode($result);
 };
 
 //Get token
 $token=sendHTTPrequest($curl, $auth_url, $auth_data)->Token;
-// $token=json_decode($token)->Token;
 
-$allActivities=array();
-
-if(!$token){die("Connection Failure");}else{
+if (!$token) {
+    die("Connection Failure");
+} else {
     
     //If the token is obtained, then make the request to the API
     $auth_data = array('Authorization: Bearer '.$token);
+    
+    //Get all locations
     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/locations';
+    $locations=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
     
-	$locations=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
-	
-    
+    //Get all programs
     $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs?showHidden=True';
-	$programs=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
+    $programs=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
 
-	//For each Program, get its activities and add them as a property
-    foreach($programs as $program){
-      	$id=$program->Id;
+    //For each Program, get its activities and add them as a property
+    foreach ($programs as $program) {
+        $id=$program->Id;
 
-        if ($id==55462) {
-    		$auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities?showOccurrences=True&perPage=1000';
+        if (in_array($id, $programsToShow)) {
+            $auth_url = 'https://www.amilia.com/api/v3/fr/org/loisirsrenaudcoursol/programs/'.$id.'/activities?showOccurrences=True&perPage=1000';
             $activities=sendHTTPrequest($curl, $auth_url, $auth_data)->Items;
             
-            foreach($activities as $activity){
-                if(!empty($activity->Occurrences)){
-					//Handle Location
-					$loc=$activity->Occurrences[0]->Location;
-					if (!empty($loc)){
-						$location=getElemById($locations, $loc->Id);
-							if(!empty($location->TopParentId)){
-							$location=getElemById($locations, $location->TopParentId);     
-							} 
-							$activity->Location=$location;
-					};
-					
-					//Handle Staff
-					$staff=$activity->Occurrences[0]->Staff;
-					if (!empty($staff[0])){
-						$activity->Staff=$staff[0];
-					};
-					unset($activity->Occurrences);
-					array_push($allActivities,$activity);
-
-				};
-               
+            foreach ($activities as $activity) {
+                
+                //Handle Location
+                $loc=$activity->Occurrences[0]->Location;
+                if (!empty($loc)) {
+                    $location=getElemById($locations, $loc->Id);
+                    if (!empty($location->TopParentId)) {
+                        $location=getElemById($locations, $location->TopParentId);
+                    }
+                    $activity->Location=$location;
+                };
+                
+                //Handle Staff
+                $staff=$activity->Occurrences[0]->Staff;
+                if (!empty($staff[0])) {
+                    $activity->Staff=$staff[0];
+                };
+                unset($activity->Occurrences);
+                array_push($allActivities, $activity);
             };
-    	};
-	};
-	$output=array(
-		"allActivities" => $allActivities,
-		"locations" => $locations
-	);
-	echo json_encode($output);
+        };
+    };
 };
-function getElemById($arr,$id){
-    foreach($arr as $elem){
-        if ($elem->Id==$id){
+function getElemById($arr, $id)
+{
+    foreach ($arr as $elem) {
+        if ($elem->Id==$id) {
             return $elem;
         }
     }
@@ -84,4 +80,3 @@ function getElemById($arr,$id){
 
 
 curl_close($curl);
-?>
