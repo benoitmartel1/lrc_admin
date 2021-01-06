36a2fbcb67b91e291bfd486016d83c904a6c6cd8
Benoit Martel <martel.b@gmail.com>
2021-01-06 14:04:48 -0500

    url

--- log_check.php
+++ log_check.php
@@ -3,10 +3,10 @@
 
   if (!isset($_SESSION['username'])) {
       $_SESSION['msg'] = "You must log in first";
-      header('location: '.$_SERVER['DOCUMENT_ROOT'].'/admin/registration/login.php');
+      header('location: https:renaudcoursol.com/admin/registration/login.php');
   }
   if (isset($_GET['logout'])) {
       session_destroy();
       unset($_SESSION['username']);
-      header('location: '.$_SERVER['DOCUMENT_ROOT'].'/admin/registration/login.php');
+      header('location: https:renaudcoursol.com/admin/registration/login.php');
   }
