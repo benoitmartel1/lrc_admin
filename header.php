f8248e58384123ee0fe6974294483c166cf114cc
Benoit Martel <martel.b@gmail.com>
2021-01-06 13:59:36 -0500

    absolute path

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 		<!-- logged in user information -->
 		<?php  if (isset($_SESSION['username'])) : ?>
 			<div>Bienvenue <strong><?php echo $_SESSION['username']; ?></strong></div>
-			<div> <a href="index.php?logout='1'"><i class="fa fa-sign-out logout"></i></a> </div>
+			<div> <a href="https://renaudcoursol.com/admin/index.php?logout='1'"><i class="fa fa-sign-out logout"></i></a> </div>
 		<?php endif ?>
 	</div>
 </div>
