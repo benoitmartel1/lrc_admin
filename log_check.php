d894c62f039cc437bf92082799e1d0dd002e1302
Benoit Martel <martel.b@gmail.com>
2021-01-06 14:02:57 -0500

    server

--- log_check.php
+++ log_check.php
@@ -1,13 +1,12 @@
-<?php 
-  session_start(); 
+<?php
+  session_start();
 
   if (!isset($_SESSION['username'])) {
-  	$_SESSION['msg'] = "You must log in first";
-  	header('location: registration/login.php');
+      $_SESSION['msg'] = "You must log in first";
+      header('location: '.$_SERVER['DOCUMENT_ROOT'].'/admin/registration/login.php');
   }
   if (isset($_GET['logout'])) {
-  	session_destroy();
-  	unset($_SESSION['username']);
-  	header("location: registration/login.php");
+      session_destroy();
+      unset($_SESSION['username']);
+      header('location: '.$_SERVER['DOCUMENT_ROOT'].'/admin/registration/login.php');
   }
-  ?>
